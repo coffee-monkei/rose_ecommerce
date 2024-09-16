2d825d2728c3c63ac76e4562e51cb51f2be13be4
Rose <coffee@monkey.com>
2024-09-16 14:59:51 -0500

    cards dinamicas

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
-console.log("Fresas con crema!");
+
+const titulo= "Productos"
+ document.querySelector("h1").innerText = titulo;
+ 
 
 
 
-const titulos = document.querySelectorAll("h3");
 
-titulos.forEach((titulo, index) => {
-    if (index === 0) {
-        
-        titulo.innerText = "velas comestibles";
-    } else if (index === 1) {
-       
-        titulo.innerText = "cloro para los ojos";
-    } else if (index === 2) {
-        
-        titulo.innerText = "hacha de raskolnikoff";
-    }
+
+
+let cards= [];
+
+for (let i=1; i<=9; i++) {
+    let card = 
+    `<div class="card" style="width: 18rem;">
+    <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="carro${i}">
+    <div class="card-body">
+      <h5 class="card-title">modelo ${i}</h5>
+      <p class="card-text">único en existencia</p>
+      <a href="#" class="btn btn-primary">añadir al carrito</a>
+    </div>
+  </div>`
     
-});
 
-const descripciones = document.querySelectorAll("p");
-
-descripciones.forEach((descripcion, index) => {
-    if (index === 0) {
-        
-        descripcion.innerText = "¿que nunca has mirado a tu vela con ojos hambrientamente esperanzadores?";
-    } else if (index === 1) {
+ cards.push(card); //Añade la tarjeta generada en cada iteración (card) al arreglo cardsArray.
        
-        descripcion.innerText = "no apto para ojos azules";
-    } else if (index === 2) {
-        
-        descripcion.innerText = "la misma que uso para descapitar a la vieja arrendera";
-    }
     
-});
 
-const imagenes = document.querySelectorAll("img");
-
-imagenes.forEach((img, index) => {
-    if (index === 0) {
-        
-        img.src = "https://i.pinimg.com/564x/bf/44/f0/bf44f00a0b4511ec4ce8515d09c60df8.jpg";
-    } else if (index === 1) {
-       
-        img.src = "https://i.pinimg.com/564x/cd/51/3c/cd513c550f8efe54fb903da5e3b3f7f1.jpg";
-    } else if (index === 2) {
-        
-        img.src = "https://i.pinimg.com/564x/e5/79/40/e57940368d4fbf30eac5a80405eda816.jpg";
-    }
     
-});
-
+} 
+document.querySelector(".container").innerHTML = cards.join('');
