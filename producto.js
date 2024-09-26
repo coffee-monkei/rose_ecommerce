cb8ef1d7ea93f4aed19a49b15bb8c4d7c804cd23
Rose <coffee@monkey.com>
2024-09-26 18:17:52 -0500

    estilos

--- producto.js
+++ producto.js
@@ -10,19 +10,19 @@ class producto{
     }
   }
   
-  const producto= new producto("mercedes mango compacto suv", "color mango de una semana", "$800,000", 4, "https://external-preview.redd.it/uz7ncWMQ2orvzbz5CT5pgIHVsJ7Xs4o0OCaYiUTpw5Q.jpg?auto=webp&s=0da9ef37da608cadd4d4d3e18013e96c45660ea7" )
+  const MyCard= new producto("mercedes mango compacto suv", "color mango de una semana", "$800,000", 4, "https://external-preview.redd.it/uz7ncWMQ2orvzbz5CT5pgIHVsJ7Xs4o0OCaYiUTpw5Q.jpg?auto=webp&s=0da9ef37da608cadd4d4d3e18013e96c45660ea7" )
  
 
   let main = document.querySelector('main');
 main.innerHTML = `
     <div class="card">
-        <img src="${producto.img}" alt="${producto.titulo}">
+        <img src="${MyCard.imagen}" alt="${MyCard.titulo}">
         <div class="card-body">
-            <h1>${producto.titulo}</h1>
-            <p>${producto.detalle}</p>
-            <p>Precio: $${producto.precio}</p>
-            <p>Stock: ${producto.stock} unidades disponibles</p>
-            <a href="./index.html" class="btn btn-primary">Comprar</a>
+            <h1 >${MyCard.titulo}</h1>
+            <p>${MyCard.detalle}</p>
+            <p>Precio: $${MyCard.precio}</p>
+            <p>Stock: ${MyCard.stock} unidades disponibles</p>
+            <a href="index.html" class="btn btn-primary" > Comprar</a>
         </div>
     </div>
 `;
